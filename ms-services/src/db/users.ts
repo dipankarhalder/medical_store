8eee524ac3eb47c27a99e22a3265733b02118b62
Dipankar Halder <dipankar@Dipankars-MacBook-Pro.local>
2024-03-03 12:34:42 +0530

    added user list, update, delete func and restructure other module code

--- ms-services/src/db/users.ts
+++ ms-services/src/db/users.ts
@@ -3,33 +3,14 @@ import { IUser } from "../interface/auth";
 
 const UserSchema: Schema = new Schema(
   {
-    first_name: { 
-      type: String, 
-      required: true 
-    },
-    last_name: { 
-      type: String, 
-      required: true 
-    },
-    phone: { 
-      type: String 
-    },
-    email: { 
-      type: String, 
-      required: true 
-    },
-    password: { 
-      type: String, 
-      required: true 
-    },
-    role: {
-      type: String,
-      required: true
-    }
+    first_name: { type: String, required: true },
+    last_name: { type: String, required: true },
+    phone: { type: String },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true }
   },
-  {
-    timestamps: true,
-  }
+  { timestamps: true, }
 );
 
 export const UserModel = mongoose.model<IUser>('User', UserSchema);
