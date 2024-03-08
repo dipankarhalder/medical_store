efd297553b1dff789507a886986a702eee9db48f
Dipankar Halder <dipankar@Dipankars-MacBook-Pro.local>
2024-03-08 22:25:58 +0530

    updated code

--- ms-services/src/controllers/auth.ts
+++ ms-services/src/controllers/auth.ts
@@ -4,16 +4,16 @@ import bcrypt from 'bcryptjs';
 import { UserModel } from '../db/users';
 import { IUser } from '../interface';
 import { generateJWTtoken } from '../helper';
-import { log_schema, reg_schema } from "../validation"
-import { 
-  cookie_title, 
-  exist_user, 
-  not_email, 
-  wrong_password, 
-  something_wrong, 
-  successfully_created, 
-  successfully_login, 
-  successfully_logout, 
+import { log_schema, reg_schema } from '../validation';
+import {
+  cookie_title,
+  exist_user,
+  not_email,
+  wrong_password,
+  something_wrong,
+  successfully_created,
+  successfully_login,
+  successfully_logout,
 } from '../config/content';
 
 /* 
@@ -21,31 +21,67 @@ import {
   @endpoint: /v1/auth/signup
   @details: register
 */
-export const register = async (req: Request, res: Response) => {
-  const { first_name, last_name, email, phone, password,role } = req.body;
+export const register = async (
+  req: Request,
+  res: Response
+) => {
+  const {
+    first_name,
+    last_name,
+    email,
+    phone,
+    password,
+    role,
+  } = req.body;
 
-  const { error } = reg_schema.validate({ first_name, last_name, email, phone, password,role, });
-  if(error) {
-    return res.status(400).json({ 
-      error: error.details[0].message 
+  const { error } = reg_schema.validate({
+    first_name,
+    last_name,
+    email,
+    phone,
+    password,
+    role,
+  });
+  if (error) {
+    return res.status(400).json({
+      error: error.details[0].message,
     });
-  } 
+  }
 
   try {
-    const existing_email = await UserModel.findOne({ email });
+    const existing_email = await UserModel.findOne({
+      email,
+    });
     if (existing_email) {
-      return res.status(400).json({ 
-        msg: `${email} ${exist_user}` 
+      return res.status(400).json({
+        msg: `${email} ${exist_user}`,
       });
     }
-    
+
     const salt = await bcrypt.genSalt(10);
     const hashed = await bcrypt.hash(password, salt);
-    const user: IUser = new UserModel({ first_name, last_name, email, phone, password: hashed, role, });
+    const user: IUser = new UserModel({
+      first_name,
+      last_name,
+      email,
+      phone,
+      password: hashed,
+      role,
+    });
     await user.save();
-    return res.json({ code: 200, data: { first_name, last_name, email, phone, role }, message: successfully_created }).end();
+    return res
+      .json({
+        code: 200,
+        data: { first_name, last_name, email, phone, role },
+        message: successfully_created,
+      })
+      .end();
   } catch (error) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
   }
 };
 
@@ -54,36 +90,55 @@ export const register = async (req: Request, res: Response) => {
   @endpoint: /v1/auth/signin
   @details: login
 */
-export const login = async (req: Request, res: Response) => {
+export const login = async (
+  req: Request,
+  res: Response
+) => {
   const { email, password } = req.body;
-  
-  const { error } = log_schema.validate({ email, password });
-  if(error) {
-    return res.status(400).json({ 
-      error: error.details[0].message 
+
+  const { error } = log_schema.validate({
+    email,
+    password,
+  });
+  if (error) {
+    return res.status(400).json({
+      error: error.details[0].message,
     });
   }
 
   try {
     const current_user = await UserModel.findOne({ email });
     if (!current_user) {
-      return res.status(400).json({ 
-        msg: `${email} ${not_email}` 
+      return res.status(400).json({
+        msg: `${email} ${not_email}`,
       });
     }
-    
-    const valid_password = await bcrypt.compare(password, current_user.password);
+
+    const valid_password = await bcrypt.compare(
+      password,
+      current_user.password
+    );
     if (!valid_password) {
-      return res.status(400).json({ 
-        msg: wrong_password 
+      return res.status(400).json({
+        msg: wrong_password,
       });
     }
 
     const token = generateJWTtoken(email);
     res.cookie(cookie_title, token, { httpOnly: true });
-    return res.json({ code: 200, data: token, message: successfully_login }).end();
+    return res
+      .json({
+        code: 200,
+        data: token,
+        message: successfully_login,
+      })
+      .end();
   } catch (error) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
   }
 };
 
@@ -92,7 +147,16 @@ export const login = async (req: Request, res: Response) => {
   @endpoint: /v1/auth/logout
   @details: logout
 */
-export const logout = async (req: Request, res: Response) => {
+export const logout = async (
+  req: Request,
+  res: Response
+) => {
   res.clearCookie(cookie_title);
-  return res.json({ code: 200, data: null, message: successfully_logout }).end();
-}
+  return res
+    .json({
+      code: 200,
+      data: null,
+      message: successfully_logout,
+    })
+    .end();
+};
--- ms-services/src/controllers/slots.ts
+++ ms-services/src/controllers/slots.ts
@@ -2,15 +2,14 @@ import { Request, Response } from 'express';
 
 import { SlotsModel } from '../db/slots';
 import { ISlot } from '../interface';
-import { slots_schema } from "../validation"
-import { 
+import { slots_schema } from '../validation';
+import {
   empty_list,
-  exist_item, 
-  lists_of_items, 
-  successfully_created, 
-  successfully_updated, 
-  successfully_deleted, 
-  something_wrong 
+  lists_of_items,
+  successfully_created,
+  successfully_updated,
+  successfully_deleted,
+  something_wrong,
 } from '../config/content';
 
 /* 
@@ -18,92 +17,150 @@ import {
   @endpoint: /v1/slots
   @details: all slots list
 */
-export const get_all_slots = async (req: Request, res: Response) => {
-  console.log("sdkjfhhjsdgfhjg");
-  try{
+export const get_all_slots = async (
+  req: Request,
+  res: Response
+) => {
+  try {
     const slots = await SlotsModel.find();
-    return res.json({ code: 200, data: slots, message: !slots ? lists_of_items : empty_list }).end();
-  } catch(err) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+    return res.json({
+      code: 200,
+      data: slots,
+      message: !slots ? lists_of_items : empty_list,
+    });
+  } catch (err) {
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
   }
-}
+};
 
 /* 
   @method: GET
   @endpoint: /v1/slot/:id
   @details: view slot
 */
-export const view_slot = async (req: Request, res: Response) => {
-  try{
+export const view_slot = async (
+  req: Request,
+  res: Response
+) => {
+  try {
     const { slotid } = req.params;
     const view_info = await SlotsModel.findById(slotid);
-    return res.json({ code: 200, data: view_info, message: `${successfully_deleted}` }).end();
-  } catch(err) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+    return res.json({
+      code: 200,
+      data: view_info,
+      message: `${successfully_deleted}`,
+    });
+  } catch (err) {
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
   }
-}
+};
 
 /* 
   @method: POST
   @endpoint: /v1/slot
   @details: create slot
 */
-export const create_slots = async (req: Request, res: Response) => {
+export const create_slots = async (
+  req: Request,
+  res: Response
+) => {
   const { slot_name } = req.body;
 
   const { error } = slots_schema.validate({ slot_name });
-  if(error) {
-    return res.status(400).json({ 
-      error: error.details[0].message 
+  if (error) {
+    return res.status(400).json({
+      error: error.details[0].message,
     });
-  } 
-  
-  try{
-    const slot: ISlot = new SlotsModel({ slot_name: `${slot_name.split(' ').join('-')}-${new Date().toISOString().split('T')[0]}` });
-    await slot.save();
-    return res.json({ code: 200, data: { slot }, message: successfully_created }).end();
-  } catch(err) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
   }
-}
+
+  try {
+    const slot: ISlot = new SlotsModel({
+      slot_name: `${slot_name.split(' ').join('-')}-${new Date().toISOString().split('T')[0]}`,
+    });
+    await slot.save();
+    return res.json({
+      code: 200,
+      data: { slot },
+      message: successfully_created,
+    });
+  } catch (err) {
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
+  }
+};
 
 /* 
   @method: PATCH
   @endpoint: /v1/slot/:id
   @details: update slot
 */
-export const update_slot = async (req: Request, res: Response) => {
+export const update_slot = async (
+  req: Request,
+  res: Response
+) => {
   const { slot_name } = req.body;
-  
-  const { error } = slots_schema.validate({ slot_name });
-  if(error) {
-    return res.status(400).json({ 
-      error: error.details[0].message 
-    });
-  } 
 
-  try{
-    const { slotid } = req.params;
-    await SlotsModel.findByIdAndUpdate(slotid, { slot_name: `${slot_name.split(' ').join('-')}-${new Date().toISOString().split('T')[0]}` });
-    const updated_info = await SlotsModel.findById(slotid);
-    return res.json({ code: 200, data: updated_info, message: successfully_updated }).end();
-  } catch(err) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+  const { error } = slots_schema.validate({ slot_name });
+  if (error) {
+    return res.status(400).json({
+      error: error.details[0].message,
+    });
   }
-}
+
+  try {
+    const { slotid } = req.params;
+    await SlotsModel.findByIdAndUpdate(slotid, {
+      slot_name: `${slot_name.split(' ').join('-')}-${new Date().toISOString().split('T')[0]}`,
+    });
+    const updated_info = await SlotsModel.findById(slotid);
+    return res.json({
+      code: 200,
+      data: updated_info,
+      message: successfully_updated,
+    });
+  } catch (err) {
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
+  }
+};
 
 /* 
   @method: DELETE
   @endpoint: /v1/slot/:id
   @details: delete slot
 */
-export const remove_slot = async (req: Request, res: Response) => {
-  try{
+export const remove_slot = async (
+  req: Request,
+  res: Response
+) => {
+  try {
     const { slotid } = req.params;
     const view_info = await SlotsModel.findById(slotid);
     await SlotsModel.findOneAndDelete({ _id: slotid });
-    return res.json({ code: 200, data: null, message: `${view_info.slot_name} ${successfully_deleted}` }).end();
-  } catch(err) {
-    return res.json({ code: 400, data: null, message: something_wrong, });
+    return res.json({
+      code: 200,
+      data: null,
+      message: `${view_info.slot_name} ${successfully_deleted}`,
+    });
+  } catch (err) {
+    return res.json({
+      code: 400,
+      data: null,
+      message: something_wrong,
+    });
   }
-}
+};
